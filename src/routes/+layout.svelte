330179910c5b6db20569a0603280cafd68156dea
trixky <mabois@student.42.fr>
2024-07-06 16:01:44 +0200

    [Dev] first commit

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,15 @@
+<!-- ================================================= SCRIPT -->
+<script lang="ts">
+  import "../styles/reset.css";
+  import "../styles/tailwind.css";
+  import "../styles/main.css";
+</script>
+
+<!-- ================================================= CONTENT -->
+<main class="flex flex-col justify-center items-center p-4">
+  <slot />
+</main>
+
+<!-- ================================================= CSS -->
+<style lang="postcss">
+</style>

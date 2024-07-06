aa9c2ca627dc63bc018311377b4c06b8e3bf2a1a
trixky <mabois@student.42.fr>
2024-07-06 22:09:25 +0200

    [Dev] footer: make the tensorflow js link in target blank

--- src/lib/components/layout/footer.svelte
+++ src/lib/components/layout/footer.svelte
@@ -5,7 +5,7 @@
 
 <!-- ================================================= CONTENT -->
 <footer class="mt-auto select-none">
-  <a href="https://www.tensorflow.org/js?hl=fr">
+  <a href="https://www.tensorflow.org/js?hl=fr" target="_blank">
     <div class="flex justify-center items-center gap-3 p-4">
       <p class="opacity-50">made with</p>
       <img src={base + "/tensorflow-js.png"} width="60" alt="tensorflow js logo" />
